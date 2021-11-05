36e711f30e12648240802f650913e6b5045e44ed
Caedan <caedanlavender@gmail.com>
2021-11-06 08:46:17 +1300

    reworked design, adjusted autocomplete behvaiour

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ function App() {
 	}
 
 	const handleEnter = (e) => {
-		console.log(e)
 		if (e.charCode === 13) handleSearch();
+		setAutocomplete([])
 	}
 
 	const handleSearch = () => {
@@ -47,46 +47,55 @@ function App() {
 		}
 	}
 
+	const openInNewTab = (url) => {
+		const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+		if (newWindow) newWindow.opener = null
+	}
+
 	return (
 		<div className="App">
-			<h1>Turners FAQ Search</h1>
+			<div className="upper">
+				<div className='title'>Search Turners</div>
+				<div className='searchContainer'>
+					<div className='searchContainer__searchWrapper'>
+						<SearchSVG className='searchIcon' />
+						<input
+							type="text"
+							value={searchTerm}
+							onChange={(e) => {
+								setSearchTerm(e.target.value)
+								debounced(e.target.value)
+							}}
+							onKeyPress={handleEnter}
+						/>
+					</div>
+					<div className={`searchContainer__autocomplete ${(autocomplete[0]) || "hidden"}`}>
+						{
+							autocomplete.map((item) => (
+								<div className='searchContainer__autocomplete__item'
+									onClick={() => {
+										setSearchTerm(item)
+										setAutocomplete([])
+									}}>
+									{item}
+								</div>
+							))
+						}
+					</div>
+				</div>
 
-			<div className="searchContainer">
-				<div className="searchContainer__searchWrapper">
-					<SearchSVG className="searchIcon" />
-					<input
-						type="text"
-						value={searchTerm}
-						onChange={(e) => {
-							setSearchTerm(e.target.value)
-							debounced(e.target.value)
-						}}
-						onKeyPress={handleEnter}
-					/>
-				</div>
-				<div className={`searchContainer__autocomplete ${searchTerm || "hidden"}`}>
-					{
-						autocomplete.map((item) => (
-							<div className='searchContainer__autocomplete__item'
-								onClick={() => {
-									setSearchTerm(item)
-									setAutocomplete([])
-								}}>
-								{item}
-							</div>
-						))
-					}
-				</div>
 			</div>
-			{/* <button onClick={handleSearch}>Go</button> */}
-			<div>
-				<h2>Search results</h2>
-				<div className={'searchResultContainer'}>
+			<div className='lower'>
+				{/* <h2>Search results</h2> */}
+				<div className='searchResultContainer'>
+					<div className="gradient">
+
+					</div>
 					{
 						searchResults.map((item) => (
-							<div className={'searchResultItem'}>
-								<h3>{item.title}</h3>
-								<div className={'searchResultItem__text'}>{item.text[0]}</div>
+							<div className='searchResultItem' onClick={() => openInNewTab(item.metadata.source.url)}>
+								<h3>{item.title[0].length > 150 ? item.title[0].slice(0, 147) + "..." : item.title}</h3>
+								<div className='searchResultItem__text'><p>{item.text[0]}</p><br/><p className="searchResultItem__link">Source: {item.metadata.source.url}</p></div>
 							</div>
 						))
 					}
